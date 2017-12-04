ecfc08849435e5eb49e793efe7b9ddfc543de5e0
Logan Jones <loganjones512@gmail.com>
2017-12-03 23:04:52 -0600

    added htons to len in TODO 1

--- router/sr_router.c
+++ router/sr_router.c
@@ -199,7 +199,7 @@ void sr_handle_arpreq(struct sr_instance *sr, struct sr_arpreq *req,
 		  icmp_ip->ip_v = 4;
 		icmp_ip->ip_hl = (sizeof(sr_ip_hdr_t)/4);
           icmp_ip->ip_tos = iphdr->ip_tos;
-          icmp_ip->ip_len = sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t);
+          icmp_ip->ip_len = htons(sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t));
           icmp_ip->ip_id = iphdr->ip_id;
           icmp_ip->ip_off = 0x0000;
           icmp_ip->ip_ttl = 0xFF;
