79acf45f0200dfbe46d1e4d0152f9f247a4beef3
Logan Jones <loganjones512@gmail.com>
2017-12-03 22:36:39 -0600

    printing headers of received packets

--- router/sr_router.c
+++ router/sr_router.c
@@ -387,7 +387,7 @@ void sr_handlepacket(struct sr_instance* sr,
   }
 
   sr_ethernet_hdr_t *ehdr = (sr_ethernet_hdr_t *) packet;
-  fprintf(stderr,"ether type is\n" + ehdr->ether_type);
+  print_hdrs(packet,len);
     
   /* Determine if packet is ARP or IP */
     switch(ehdr->ether_type){
